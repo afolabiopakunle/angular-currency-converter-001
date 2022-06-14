b9afecb455b0f69d25e65fb53b326cbd44bac677
afolabiopakunle <opakunleafolabi@gmail.com>
2022-06-14 10:45:40 +0100

    bootstrap added

---
+++ src/app/naira/naira.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-naira',
+  templateUrl: './naira.component.html',
+  styleUrls: ['./naira.component.css']
+})
+export class NairaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
