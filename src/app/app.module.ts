9f50b68fd82831a5324abef267bfa77786b33532
afolabiopakunle <opakunleafolabi@gmail.com>
2022-06-14 15:21:19 +0100

    naira input added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NairaComponent } from './naira/naira.component';
+import { DollarComponent } from './dollar/dollar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NairaComponent, DollarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
